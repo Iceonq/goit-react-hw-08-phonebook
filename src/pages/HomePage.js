25747653b435c138664801db2675183eb676bc79
Iceonq <bartek.sosnowicz@gmail.com>
2023-05-18 18:59:10 +0200

    .

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,3 @@
-import { ContactsList } from 'components/ContactsList/ContactsList';
-import { Form } from 'react-router-dom';
-
 export const HomePage = () => {
   return (
     <div>This is homepage</div>
